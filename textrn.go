06788647dc93e3cfeccff2ac310db1bfb3ca0189
Nathan Au <nathan.au1280@gmail.com>
2021-05-19 17:26:59 -0400

    Making a client type

--- textrn.go
+++ textrn.go
@@ -15,10 +15,19 @@ var (
     connect_sid = os.Getenv("TEXTRN_CONNECT_SID")
     username = os.Getenv("TEXTRN_USERNAME")
     phone = os.Getenv("TEXTRN_PHONE")
+
+    // Base URL for TextNow service
     base_url = "https://www.textnow.com/api/users/"
+
     // Client used for caching TCP connections
     client = &http.Client{}
 )
+
+type Client struct {
+    ConnectSid  string
+    Username    string
+}
+
 
 func GetMessages() {
     // Create client and request objects
@@ -54,7 +63,15 @@ type SendMessageBody struct {
     Date                string  `json:"date"`
 }
 
-func SendMessage(number, message string) {
+type MessageId struct {
+    Id  string  `json:"id"`
+}
+
+/*
+    Sends an SMS message to the specified number
+*/
+func (c Client) SendMessage(number, message string) string {
+    // Construct message body
     reqBody := SendMessageBody{
         number,
         1,
@@ -62,50 +79,48 @@ func SendMessage(number, message string) {
         1,
         2,
         1,
-        username,
+        c.Username,
         false,
         true,
         time.Now().Format(time.RFC3339),
     }
+
+    // Convert body to JSON
     bytes, err := json.Marshal(reqBody)
     if err != nil {
         fmt.Println("err")
-        return
+        return "err"
     }
+
+    // Create HTTP POST request object
     req, err := http.NewRequest("POST", base_url + username + "/messages", strings.NewReader(string(bytes)))
     if err != nil {
         fmt.Println("err")
-        return
+        return "err"
     }
-    req.Header.Set("Cookie", "connect.sid=" + connect_sid)
+
+    // Set appropriate headers
+    // The Cookie header containing the connect.sid is required for authentication with the service
+    req.Header.Set("Cookie", "connect.sid=" + c.ConnectSid)
     req.Header.Set("Content-Type", "application/json")
     req.Header.Set("Host", "www.textnow.com")
     req.Header.Set("Connection", "keep-alive")
 
+    // Perform HTTP request
     res, err := client.Do(req)
     if err != nil {
         fmt.Println("err")
-        return
+        return "err"
     }
+
+    // Read response data
     defer res.Body.Close()
     data, err := ioutil.ReadAll(res.Body)
     if err != nil {
         fmt.Println("err")
-        return
+        return "err"
     }
-    // Print response (id)
-    fmt.Println(string(data))
+
+    return string(data)
 }
 
-/*
-func main() {
-
-    msg := flag.String("msg", "", "A string.")
-    flag.Parse()
-    if *msg == "" {
-        fmt.Println("No message passed in")
-        return
-    }
-    SendMessage(phone, *msg)
-}
-*/
